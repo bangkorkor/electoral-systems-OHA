2c66094b2f07a373732836b30b0e34e1e03175f1
Alessandro Zito <alessandro4.zito@mail.polimi.it>
2021-11-18 17:52:31 +0100

    feat: works for "Estero"

--- src/Commons/binomiale.py
+++ src/Commons/binomiale.py
@@ -41,6 +41,75 @@ def binomiale_valle_daosta(*a, data, **kwargs):
     return dataframe_aosta
 
 
-# file1 = pd.read_csv('/home/poliradio/AleZito/SimulatoreSistemiElettorali-2/LeggiElettorali/Binomiale/Data/Valle_Aosta/voti_valle_d_aosta.csv')
-# data = file1.copy()
-# binomiale_valle_daosta(data = data)
+def binomiale_estero(*a, data, **kwargs):
+
+    dataframe_estero = data.copy()
+    dataframe_estero['Seggi'] = 0
+    # print(dataframe_estero)
+
+
+    #list(set(dataframe_estero["Ente"]))
+
+    enti = list(set(dataframe_estero["Ente"]))
+    enti.sort()
+    print(enti)
+
+    liste = dataframe_estero.groupby(['Ente'])['Lista'].apply(list)
+    voti = dataframe_estero.groupby(['Ente'])['Voti'].apply(list)
+
+    # print(list(set(dataframe_estero["Ente"])))
+    #dataframe = dataframe_estero.groupby(['Ente'])['Lista'].apply(list)
+    #print(dataframe)
+
+    
+    dataframe_estero = pd.DataFrame(columns = ['Ente', 'Lista', 'Voti', 'Seggi'])
+    
+
+    for ente in enti:
+        # print(liste[ente])
+        # print(voti[ente])
+
+        if (voti[ente][0] < voti[ente][1] * 2):
+            dataframe_estero = dataframe_estero.append({'Ente': ente, 'Lista': liste[ente][0], 'Voti': voti[ente][0], 'Seggi': 1}, ignore_index=True)
+            dataframe_estero = dataframe_estero.append({'Ente': ente, 'Lista': liste[ente][1], 'Voti': voti[ente][1], 'Seggi': 1}, ignore_index=True)
+        else:
+            dataframe_estero = dataframe_estero.append({'Ente': ente, 'Lista': liste[ente][0], 'Voti': voti[ente][0], 'Seggi': 2}, ignore_index=True)           
+
+    print(dataframe_estero)
+        
+
+        # subdataframe = dataframe_estero[dataframe_estero["Ente"] == ente]
+        # voti_primo = subdataframe.iloc[0, subdataframe.columns.get_loc('Voti')]
+        # voti_secondo = subdataframe.iloc[1, subdataframe.columns.get_loc('Voti')]
+    sys.exit()
+
+    # subdataframe = dataframe_estero[dataframe_estero["Ente"] == 'RESTO DEL MONDO']
+    # print(subdataframe)
+    
+    # voti_primo = subdataframe.iloc[0, subdataframe.columns.get_loc('Voti')]
+    # voti_secondo = subdataframe.iloc[1, subdataframe.columns.get_loc('Voti')]
+    # index_primo = subdataframe.index[subdataframe['Ente'] == 'RESTO DEL MONDO'].tolist()
+
+    # print(index_primo)
+    sys.exit()
+
+    # The system works in the following manner: Parties and independent candidates group themselves into lists or coalitions, basically electoral blocs. 
+    # Each list proposes up to two candidates per electoral region, province, or other geographical unit. 
+    # Votes are first tallied by list instead of by candidate, and unless the list which obtained the first majority has double the voting 
+    # as the second majority, each of the two lists gets one of their candidates, the one who got the most voting, into office
+    
+    if(voti_primo < voti_secondo * 2):
+        subdataframe.iloc[0:2, subdataframe.columns.get_loc('Seggi')] = 1
+        subdataframe.iloc[2:, subdataframe.columns.get_loc('Seggi')] = 0
+    else:
+        subdataframe.iloc[0, subdataframe.columns.get_loc('Seggi')] = 2
+        subdataframe.iloc[1:, subdataframe.columns.get_loc('Seggi')] = 0
+
+    print(subdataframe)
+    sys.exit()
+
+    return dataframe_estero
+
+file1 = pd.read_csv('/home/poliradio/AleZito/SimulatoreSistemiElettorali-2/LeggiElettorali/Binomiale/Data/Circoscrizione_Estera/voti_estero.csv')
+data = file1.copy()
+binomiale_estero(data = data)
