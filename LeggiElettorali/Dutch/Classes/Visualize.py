bc3446fe454b5e4527d010f93dfa85038aae68a5
Henrik Bang-Olsen <henrikbang01@gmail.com>
2024-05-21 23:21:41 +0200

    Visualization class added, colors too simular

---
+++ LeggiElettorali/Dutch/Classes/Visualize.py
@@ -0,0 +1,74 @@
+import json
+import os
+import plotly.graph_objects as go
+from Classes.Mandates_distribution import Mandates_distribution
+import matplotlib.pyplot as plt
+import poli_sci_kit
+import matplotlib.patches as mpatches
+
+
+
+class Visualize:
+    def __init__(self, mandates,instance):
+        current_directory_path = os.path.dirname(__file__)
+
+        self.mandate_distribution = mandates
+        self.party_colors = self.get_party_colors(current_directory_path + "/../Data/" + instance["data"]["colors"])
+        print(self.party_colors)
+        #self.parties = list(self.mandate_distribution.keys())
+        #self.mandates = list(self.mandate_distribution.values())
+
+
+    def get_party_colors(self, file_path):
+        with open(file_path, 'r') as file:
+            colors = json.load(file)
+        return colors
+        
+
+    def get_remove_losers(self, mandate_list):
+        # remove parties that have zero mandates
+        parties_suitable = {}
+        for party, mandate in mandate_list.items():
+            if mandate > 0:
+                parties_suitable[party] = mandate
+        return parties_suitable
+
+    def show_dot_chart(self):
+        parties = list(self.get_remove_losers(self.mandate_distribution).keys())
+        mandates = list(self.get_remove_losers(self.mandate_distribution).values())
+
+
+        # COLORS, keep only parties that have mandates
+        colors = []
+        for elm in self.party_colors:
+            if elm in parties:
+                colors.append(self.party_colors[elm])
+        print(colors)
+
+        print("len(parties)", len(parties))
+        print("len(colors)", len(colors))
+        
+
+        fig, ax2 = plt.subplots(figsize=(10,7))  # Only ax2 subplot
+
+        ax2 = poli_sci_kit.plot.parliament(
+            allocations=mandates,
+            labels=parties,
+            colors=colors,
+            style="semicircle",
+            num_rows=12,
+            marker_size=60,
+            speaker=False,
+            axis=ax2,
+)
+
+        # Create a legend for party colors
+        legend_patches = [mpatches.Patch(color=color, label=party) for party, color in zip(parties, colors)]
+        ax2.legend(handles=legend_patches, loc='upper left')
+
+        plt.show()
+
+
+    
+        
+        
